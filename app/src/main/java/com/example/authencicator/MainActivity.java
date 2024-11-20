bd393adcb21cbaa1fdc5add976f57bc9748177c8
Pasha883 <pashaboss883@gmail.com>
2024-11-20 20:58:27 +0300

    Counter added

--- app/src/main/java/com/example/authencicator/MainActivity.java
+++ app/src/main/java/com/example/authencicator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.authencicator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String name = "АдскийКиллер228";
     public static final String password = "Qwerty123";
+    public static int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +29,26 @@ public class MainActivity extends AppCompatActivity {
         Button but = findViewById(R.id.button);
 
         but.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("DefaultLocale")
             @Override
             public void onClick(View v) {
                 String s = namee.getText().toString();
                 String s2 = pass.getText().toString();
-
-                if (s.equals(name) && s2.equals(password)){
-                    Toast toast;
-                    toast = Toast.makeText(v.getContext(), "Welcome, " + name, Toast.LENGTH_SHORT);
-                    toast.show();
+                if(counter > 0) {
+                    if (s.equals(name) && s2.equals(password)) {
+                        Toast toast;
+                        toast = Toast.makeText(v.getContext(), "Welcome, " + name, Toast.LENGTH_SHORT);
+                        toast.show();
+                    } else {
+                        Toast toast;
+                        toast = Toast.makeText(v.getContext(), String.format("Wrong, you have %d attempts left", counter), Toast.LENGTH_SHORT);
+                        toast.show();
+                        counter--;
+                    }
                 }
                 else{
                     Toast toast;
-                    toast = Toast.makeText(v.getContext(), "Wrong, You have 3 attempts left", Toast.LENGTH_SHORT);
+                    toast = Toast.makeText(v.getContext(), "You have no attempts left", Toast.LENGTH_SHORT);
                     toast.show();
                 }
             }
